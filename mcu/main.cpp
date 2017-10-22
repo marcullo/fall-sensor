70c8e295de052190ae3bdaeb3ad9f16792e6433c
Lukasz Marcul <dev.marcul@gmail.com>
2017-10-22 19:44:15 +0200

    Squashed merge with develop
    * initialized project description
    
    * initial functionality
    
    * LV: edited data frame, deleted asking for data
    
    * edited imu config, changed uart pins and baud rate, data cumulated in ram

--- mcu/main.cpp
+++ mcu/main.cpp
@@ -1,71 +1,101 @@
 #include "mbed.h" /* Release 150 */
 #include "sdk/hardware/mpu9255.h"
+#include "tools/error_handler.h"
 
-#define MODE_DISPLAY 0
-#define MODE_SAVE 1
-
+DigitalIn userButton(USER_BUTTON);
 DigitalOut successLed(LED1);
-Serial pc(SERIAL_TX, SERIAL_RX);
+Serial pc(PA_9, PA_10);
+Ticker t;
+volatile bool processing_next_data_allowed;
 
-int main()
+/* 8000 packets (acc x,y,z; gyro x,y,z 2B) */
+uint8_t persistent_data[96000];
+
+bool isButtonPressed()
 {
-    pc.baud(921600);
-    mpu9255_init();
-    
-    uint8_t data_frame[13];
-    data_frame[12] = '\0';
-    
-    uint8_t app_mode = 0;    
-    while(!pc.readable())
-        ;
-    app_mode = pc.getc() - '0';
-    
-    switch(app_mode)
+    return userButton.read() == 0;
+}
+
+void allow_processing_data()
+{
+    if (!processing_next_data_allowed)
     {
-        case MODE_DISPLAY:
-            while(1)
-            {
-                mpu9255_accel_read_data_regs(data_frame);
-                mpu9255_gyro_read_data_regs(data_frame+6);
-                
-                pc.printf("a%db%dc%d", 
-                        (int16_t)((data_frame[0] << 8) + data_frame[1]),
-                        (int16_t)((data_frame[2] << 8) + data_frame[3]),
-                        (int16_t)((data_frame[4] << 8) + data_frame[5]));
-                
-                pc.printf("d%de%df%d", 
-                        (int16_t)((data_frame[6] << 8) + data_frame[7]),
-                        (int16_t)((data_frame[8] << 8) + data_frame[9]),
-                        (int16_t)((data_frame[10] << 8) + data_frame[11]));
-                
-                pc.printf("\r");
-            }
-            break;
-            
-        case MODE_SAVE:
-            while(1)
-            {
-                mpu9255_accel_read_data_regs(data_frame);
-                mpu9255_gyro_read_data_regs(data_frame+6);
-                
-                pc.printf("%d %d %d ", 
-                        (int16_t)((data_frame[0] << 8) + data_frame[1]),
-                        (int16_t)((data_frame[2] << 8) + data_frame[3]),
-                        (int16_t)((data_frame[4] << 8) + data_frame[5]));
-                
-                pc.printf("%d %d %d", 
-                        (int16_t)((data_frame[6] << 8) + data_frame[7]),
-                        (int16_t)((data_frame[8] << 8) + data_frame[9]),
-                        (int16_t)((data_frame[10] << 8) + data_frame[11]));
-                
-                pc.printf("\r");
-            }
-            break;
-            
-        default: 
-            pc.printf("Invalid app mode!");
-            while(1)
-                ;
-            break;
+        processing_next_data_allowed = true;
+    }
+    else
+    {
+        /* data reading from imu sensor is too slow */
+        process_error();
     }
 }
+
+void record_persistent_data(uint8_t* data, uint32_t len)
+{
+    if (!data || !len)
+        process_error();
+        
+    t.attach(&allow_processing_data, 0.001);
+        
+    uint32_t data_cnt = 0;
+    while(data_cnt < len)
+    {
+        if(!processing_next_data_allowed)
+            continue;
+        
+        mpu9255_accel_read_data_regs(data + data_cnt);
+        data_cnt += 6;
+        mpu9255_accel_read_data_regs(data + data_cnt);        
+        data_cnt += 6;
+        
+        processing_next_data_allowed = false;
+    }
+    
+    t.detach();
+}
+
+void send_persistent_packet(uint8_t* data, uint32_t len)
+{
+    if (!data || !len)
+        process_error();
+     
+    uint32_t data_cnt = 0;
+    while(data_cnt < len)
+    {        
+        pc.printf("%04x%04x%04x",
+                (uint16_t)((data[data_cnt + 0] << 8) + data[data_cnt + 1]),   
+                (uint16_t)((data[data_cnt + 2] << 8) + data[data_cnt + 3]),   
+                (uint16_t)((data[data_cnt + 4] << 8) + data[data_cnt + 5]));  
+        data_cnt += 6;
+                
+        pc.printf("%04x%04x%04x",
+                (uint16_t)((data[data_cnt + 6] << 8) + data[data_cnt + 7]),   
+                (uint16_t)((data[data_cnt + 8] << 8) + data[data_cnt + 9]),   
+                (uint16_t)((data[data_cnt + 10] << 8) + data[data_cnt + 11]));
+        data_cnt += 6;
+        
+        pc.printf("\r");
+    }
+}
+
+int main()
+{    
+    pc.baud(115200);
+    mpu9255_init();
+        
+    while(!isButtonPressed())
+        ;
+    
+    record_persistent_data(persistent_data, sizeof(persistent_data));
+    
+    successLed.write(1);
+    
+    while(!isButtonPressed())
+        ;
+    send_persistent_packet(persistent_data, sizeof(persistent_data));
+    
+    successLed.write(0);
+    
+    while(1)
+        ;
+}
+
